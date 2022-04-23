f1804d520e4aa78915e2c3c8f9d0e7aa3b4a49be
Chara- <jccharalopez@gmail.com>
2022-04-22 21:32:29 -0500

    chore: update lint-staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,12 +1,11 @@
 module.exports = {
-  '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
-
   // This will lint and format TypeScript and JavaScript files
-  '**/*.(ts|tsx|js)': (filenames) => [
-    `yarn lint --fix ${filenames.join(' ')}`,
-    `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.{js,jsx,ts,tsx}': () => [
+    `yarn tsc --noEmit`,
+    `yarn prettier`,
+    `yarn lint --max-warnings 0`,
   ],
 
   // this will Format MarkDown and JSON
-  '**/*.(md|json)': (filenames) => `yarn prettier --write ${filenames.join(' ')}`,
+  '**/*.{md,json}': () => `yarn prettier`,
 }
