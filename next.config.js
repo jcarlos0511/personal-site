c417ee850b1221f5e47a7189dbea6bc712fb1993
Chara- <jccharalopez@gmail.com>
2022-04-30 00:13:25 -0500

    feat: add security headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,49 @@
 /** @type {import('next').NextConfig} */
+
+// More info https://nextjs.org/docs/advanced-features/security-headers
+const ContentSecurityPolicy = `
+  object-src 'none';
+`
+
+const securityHeaders = [
+  { key: 'X-Frame-Options', value: 'DENY' },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+]
+
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
 }
 
 module.exports = nextConfig
