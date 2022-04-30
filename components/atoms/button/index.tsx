f028e3543871d98dd8ac2fcd3e38e7dee853a467
Chara- <jccharalopez@gmail.com>
2022-04-29 23:56:05 -0500

    feat: automate chromatic with github actions

--- components/atoms/button/index.tsx
+++ components/atoms/button/index.tsx
@@ -3,5 +3,5 @@ import { ButtonHTMLAttributes } from 'react'
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export const Button = (props: ButtonProps) => {
-  return <button {...props}>Button.tsx</button>
+  return <button {...props}>Button label changes.tsx</button>
 }
